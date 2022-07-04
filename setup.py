cea6ae43bb5a7f334a9f73d810875a9b9497ea9c
Alexandre Maurel <alexandre.maurel@gmail.com>
2022-07-04 16:09:58 +0200

    add setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
+#
+# setup(name='pytson',
+#       version='1.0',
+#       py_modules=[],
+#       python_requires='>=3.6',
+#       )
 
-setup(name='pytson',
-      version='1.0',
-      py_modules=[],
-      python_requires='>=3.6',
-      )
+from setuptools import setup, find_packages
+
+setup(
+    name='pytson', version='1.0',
+    packages=find_packages(),
+)
