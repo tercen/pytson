2f82c5e671946c6e7a2b55ff3c40427df1003cd7
Alexandre Maurel <alexandre.maurel@gmail.com>
2022-07-12 14:47:36 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-# from distutils.core import setup
-#
-# setup(name='pytson',
-#       version='1.0',
-#       py_modules=[],
-#       python_requires='>=3.6',
-#       )
-
 from setuptools import setup, find_packages
 
 setup(
