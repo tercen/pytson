306677ce3c6a2017d55748c0e3e6f1c52cdcece0
Thiago Monteiro <tsmonteiro@gmail.com>
2023-03-02 16:10:17 -0300

    Expose classes

--- pytson/__init__.py
+++ pytson/__init__.py
@@ -1,7 +1,7 @@
 # Import functions from all modules
-from pytson.serializer import Serializer
+from pytson.serializer import Serializer, SerializerIt, SerializerJsonIterator
 from pytson.deserializer import DeSerializer
 from .pytson import encodeTSON, decodeTSON
 
 # Define version
-__version__ = "0.0.1"
+__version__ = "1.7.4"
